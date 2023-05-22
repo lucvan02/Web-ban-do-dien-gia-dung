5f0017dfd46b90be8acc23b0f1960864dcd601b5
Văn Lục <vanlucmc@gmail.com>
2023-05-22 09:55:31 +0700

    them donhang danhgia

--- webBanDienGiaDung/src/main/java/ptithcm/controller/mainController.java
+++ webBanDienGiaDung/src/main/java/ptithcm/controller/mainController.java
@@ -18,45 +18,57 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import ptithcm.Entity.GioHangEntity;
 import ptithcm.Entity.SanPhamEntity;
-import ptithcm.service.SanPhamService;
 
 @Transactional
 @Controller
 public class mainController {
-
 	@Autowired
-	SanPhamService sanPhamService;
+	SessionFactory factory;
+	
+	public List<SanPhamEntity> laySanPhamTheoLoai(String loai){
+		Session session = factory.getCurrentSession();
+		String hql = "FROM SanPhamEntity sp WHERE sp.loaiSanPham.maLoai = :loai and trangThai=True ";
+		Query query = session.createQuery(hql).setParameter("loai", loai);
+		query.setMaxResults(6);
+		List<SanPhamEntity> list = query.list();
+		return list;
+	}
 	
 	@RequestMapping()
 	public String main(HttpServletRequest request,ModelMap model) {
-		List<SanPhamEntity> listNgauNhien = sanPhamService.laySanPhamNgauNhien();
-				
-		 model.addAttribute("sanPhamNgauNhien", listNgauNhien); 
-		 
-		 List<SanPhamEntity> listMoi = sanPhamService.laySanPhamMoi();
-		 model.addAttribute("sanPhamMoi", listMoi);
+		Session session = factory.getCurrentSession();
+		String hql = "FROM SanPhamEntity where trangThai=True ORDER BY NEWID()";
+		Query query = session.createQuery(hql);
+		query.setMaxResults(6);
+		List<SanPhamEntity> list = query.list();
+		for(int i=0;i<list.size();i++)
+		Hibernate.initialize(list.get(i).getHinhAnhs());
 		
-		List<SanPhamEntity> listTuLanh = sanPhamService.laySanPhamTheoLoai("TULANH");
+		
+		
+		 model.addAttribute("sanPhamMoi", list); 
+		
+		List<SanPhamEntity> listTuLanh = laySanPhamTheoLoai("TULANH");
 		
 		 model.addAttribute("listTuLanh", listTuLanh); 
 		
-		List<SanPhamEntity> listMayLanh = sanPhamService.laySanPhamTheoLoai("MAYLANH");
+		List<SanPhamEntity> listMayLanh = laySanPhamTheoLoai("MAYLANH");
 		
 		 model.addAttribute("listMayLanh", listMayLanh); 
 		
-		List<SanPhamEntity> listMayGiat = sanPhamService.laySanPhamTheoLoai("MAYGIAT");
+		List<SanPhamEntity> listMayGiat = laySanPhamTheoLoai("MAYGIAT");
 		
 		 model.addAttribute("listMayGiat", listMayGiat); 
 		
-		List<SanPhamEntity> listTuDong = sanPhamService.laySanPhamTheoLoai("TUDONG");
+		List<SanPhamEntity> listTuDong = laySanPhamTheoLoai("TUDONG");
 	
 		 model.addAttribute("listTuDong", listTuDong); 
 		
-		List<SanPhamEntity> listQuat = sanPhamService.laySanPhamTheoLoai("QUAT");
+		List<SanPhamEntity> listQuat = laySanPhamTheoLoai("QUAT");
 	
 		 model.addAttribute("listQuat", listQuat); 
 		
-		List<SanPhamEntity> listNoiCom = sanPhamService.laySanPhamTheoLoai("NOICOM");
+		List<SanPhamEntity> listNoiCom = laySanPhamTheoLoai("NOICOM");
 		
 		 model.addAttribute("listNoiCom", listNoiCom); 
 		 
@@ -65,6 +77,15 @@ public class mainController {
 		
 		return "main";
 	}
+	
+	@RequestMapping("khongCoQuyen")
+	public String khongCoQuyen() {
+		return "loi/khongCoQuyen";
+	}
+	
+	
+	
+	
 	
 
 }
